3f57eb4ec876cb953fa4cba991c34e71cdbe50d5
cong hoa <annhandt09@gmail.com>
2017-11-25 15:55:13 +0700

    Fix

--- Modbus_INOUT.ino
+++ Modbus_INOUT.ino
@@ -15,12 +15,21 @@ boolean led;
 int8_t state = 0;
 unsigned long tempus;
 
+long _time = 0;
+long lastDebounceTime1 = 0;
+long lastDebounceTime2 = 0;
+long lastDebounceTime3 = 0;
+long lastDebounceTime4 = 0;
+long debounceDelay = 100; 
+
 // data array for modbus network sharing
 uint16_t au16data[9];
 boolean status1=0;
 boolean status2=0;
 boolean status3=0;
 boolean status4=0;
+long lastDebounceTime[4] = {0,0,0,0};
+boolean _status[4]={0,0,0,0};
 /**
  *  Setup procedure
  */
@@ -74,59 +83,31 @@ void io_setup() {
 /**
  *  Link between the Arduino pins and the Modbus array
  */
+void get_button(byte pin, byte vitri){
+  _time=millis();
+  if ( ( _time - lastDebounceTime[vitri]) > debounceDelay) {
+    if (digitalRead(6) == LOW) {
+      if (_status[vitri]){
+      _status[vitri]=false;
+      boolean _bit = (bitRead( au16data[0], vitri+4 )==0)?1:0 ;
+      bitWrite( au16data[0], vitri+4, _bit );
+      lastDebounceTime[vitri] = millis(); 
+      }
+    }
+    else if (_status[vitri]==false) {_status[vitri]=true;lastDebounceTime[vitri] = millis(); }
+  }
+}
+
 void io_poll() {
   // get digital inputs -> au16data[0]
   bitWrite( au16data[0], 0, digitalRead( 2 ));
   bitWrite( au16data[0], 1, digitalRead( 3 ));
   bitWrite( au16data[0], 2, digitalRead( 4 ));
   bitWrite( au16data[0], 3, digitalRead( 5 ));
-  
-  if (digitalRead( 6 )==0 ){
-    if (status1){
-    status1=false;
-    boolean _bit = (bitRead( au16data[0], 4 )==0)?1:0 ;
-    bitWrite( au16data[0], 4, _bit );
-   // byte trave=bitRead( au16data[0], 4 );
-    //if (trave>0)bitWrite( au16data[0], 4, 0);
-    //else bitWrite( au16data[0], 4, 1);
-    }
-  }
-  else if (status1==false) status1=true;
-  
-  if (digitalRead( 7 )==0 ){
-    if (status2){
-    status2=false;
-    byte trave=bitRead( au16data[0], 5 );
-    if (trave>0)bitWrite( au16data[0], 5, 0);
-    else bitWrite( au16data[0], 5, 1);
-    }
-  }
-  else if (status2==false) status2=true;
-  
-  if (digitalRead( 8 )==0 ){
-    if (status3){
-    status3=false;
-    byte trave=bitRead( au16data[0], 6 );
-    if (trave>0)bitWrite( au16data[0], 6, 0);
-    else bitWrite( au16data[0], 6, 1);
-    }
-  }
-  else if (status3==false) status3=true;
-  
-  if (digitalRead( 9 )==0 ){
-    if (status4){
-    status4=false;
-   // byte trave=bitRead( au16data[0], 7 );
-    //gui = (bitRead( au16data[0], 7 )==0)?1:0 ;
-    boolean _bit = (bitRead( au16data[0], 7 )==0)?1:0 ;
-    bitWrite( au16data[0], 7, _bit );
- // gui1 = (digitalRead(IN2)==0)?0:1 ;
-  //gui2 = (digitalRead(IN3)==0)?0:1 ;  
-   // if (trave>0)bitWrite( au16data[0], 7, 0);
-   // else bitWrite( au16data[0], 7, 1);
-    }
-  }
-  else if (status4==false) status4=true;
+  get_button(6,0);
+  get_button(7,1);
+  get_button(8,2);
+  get_button(9,3);
  // Doc Coil Xuat ngo ra
   digitalWrite( 13, bitRead( au16data[0], 4 ));
   digitalWrite( 11, bitRead( au16data[0], 5 ));
